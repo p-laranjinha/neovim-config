c44b037023587b660cc2d9e43dbec209c89e55c5
p-laranjinha <plcasimiro2000@gmail.com>
2026-01-27 16:56:34 +0000

    Added :Wq command.

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,8 +1,16 @@
--- write without formatting
-vim.api.nvim_create_user_command("W", function(table)
+local write_without_formatting = function(bang)
 	vim.g.disable_autoformatting = true
-	vim.cmd.write({ bang = table.bang })
+	vim.cmd.write({ bang = bang })
 	vim.g.disable_autoformatting = false
+end
+vim.api.nvim_create_user_command("W", function(table)
+	write_without_formatting(table.bang)
+end, {
+	bang = true,
+})
+vim.api.nvim_create_user_command("Wq", function(table)
+	write_without_formatting(table.bang)
+	vim.cmd.quit({ bang = table.bang })
 end, {
 	bang = true,
 })
