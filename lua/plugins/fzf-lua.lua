ae099c72a6dc2e92f5f6cbe0c98ade1c74a746d9
p-laranjinha <plcasimiro2000@gmail.com>
2026-01-30 02:01:04 +0000

    Added notification to remind me of fzf-lua keybinds.

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -9,7 +9,17 @@ return {
 			defaults = {
 				-- copen = false,
 			},
+			winopts = {
+				on_create = function()
+					local MiniNotify = require("mini.notify")
+					local id = MiniNotify.add("Press <F1> for fzf-lua keymaps.", "INFO")
+					vim.defer_fn(function()
+						MiniNotify.remove(id)
+					end, 3000)
+				end,
+			},
 			keymap = {
+				-- These are actions native to fzf, run `man fzf` to find out more.
 				fzf = {
 					["ctrl-q"] = "select-all+accept",
 				},
