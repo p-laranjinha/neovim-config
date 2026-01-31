cbd561b59378e3d4f39078bdbdf9c3b22f92d245
p-laranjinha <plcasimiro2000@gmail.com>
2026-01-31 19:03:35 +0000

    Made smart-splits not lazy loaded.

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -1,6 +1,7 @@
 -- Better window functionality and terminal integrations.
 return {
 	"mrjones2014/smart-splits.nvim",
+	lazy = false,
 	opts = {
 		at_edge = "stop",
 		cursor_follows_swapped_bufs = true,
